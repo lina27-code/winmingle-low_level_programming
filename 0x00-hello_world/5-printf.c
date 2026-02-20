d234d9afdc58d05225dd0448231fdf8583a1cd80
Otu faith kokuwa <f79906212@gmail.com>
2026-02-20 18:38:30 +0100

    Add program that prints message using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Otu Faith Kokuwa
+ * Program: WinMingle Community C Training
+ * Description: Prints a message using printf
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
+}
