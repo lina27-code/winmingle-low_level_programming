df3a41f6c5481466a1844f2a29028858cf57daa3
Otu faith kokuwa <f79906212@gmail.com>
2026-02-20 19:18:58 +0100

    Add program that prints quote to stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Otu Faith Kokuwa
+ * Program: WinMingle Community C Training
+ * Description: Prints a quote to standard error
+ */
+
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1
+ */
+int main(void)
+{
+    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+    return (1);
+}
