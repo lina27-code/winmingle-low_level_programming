13992e92c412f2993472905f3c5dd798f1225acb
Otu faith kokuwa <f79906212@gmail.com>
2026-02-20 18:30:58 +0100

    Add program that prints message using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Otu Faith Kokuwa
+ * Program: WinMingle Community C Training
+ * Description: Prints a message using puts
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    puts("Programming is like building a multilingual puzzle");
+    return (0);
+}
