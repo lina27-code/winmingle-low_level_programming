92a7d490d66e1452ec85ba25d2ecc6832b1cd3c2
Otu faith kokuwa <f79906212@gmail.com>
2026-02-28 19:03:56 +0100

    Complete 0x02-functions_nested_loops project

---
+++ 0x02-functions_nested_loops/100-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_times_table(3);
+	_putchar('\n');
+	print_times_table(10);
+	_putchar('\n');
+	print_times_table(14);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,63 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the n times table
+ * @n: number of times table
+ */
+void print_times_table(int n)
+{
+	int row, col, result;
+
+	if (n < 0 || n > 14)
+		return;
+
+	for (row = 0; row <= n; row++)
+	{
+		for (col = 0; col <= n; col++)
+		{
+			result = row * col;
+
+			if (col == 0)
+			{
+				if (result >= 100)
+				{
+					_putchar((result / 100) + '0');
+					_putchar(((result / 10) % 10) + '0');
+					_putchar((result % 10) + '0');
+				}
+				else if (result >= 10)
+				{
+					_putchar((result / 10) + '0');
+					_putchar((result % 10) + '0');
+				}
+				else
+					_putchar(result + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				if (result < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(result + '0');
+				}
+				else if (result < 100)
+				{
+					_putchar(' ');
+					_putchar((result / 10) + '0');
+					_putchar((result % 10) + '0');
+				}
+				else
+				{
+					_putchar((result / 100) + '0');
+					_putchar(((result / 10) % 10) + '0');
+					_putchar((result % 10) + '0');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
