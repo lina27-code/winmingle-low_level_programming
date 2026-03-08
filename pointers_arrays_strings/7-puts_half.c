6e8d6a28ad2e8126949a456e61aea04b71076702
Otu faith kokuwa <f79906212@gmail.com>
2026-03-08 21:55:22 +0100

    Task 7: print second half of a string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of a string
+ * @str: pointer to the string
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+	int start;
+	int i;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	if (len % 2 == 0)
+		start = len / 2;
+	else
+		start = (len + 1) / 2;
+
+	for (i = start; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
