7ab8e46d67f6a0b8ceca18c8e6506382e9e4568e
Otu faith kokuwa <f79906212@gmail.com>
2026-03-17 22:16:19 +0100

    Task 6: Implement print_chessboard function

---
+++ 0x07-pointers_arrays_strings/6-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Prints an 8x8 chessboard
+ * @a: 2D array representing the board (8 columns)
+ *
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+    int row, col;
+
+    for (row = 0; row < 8; row++)
+    {
+        for (col = 0; col < 8; col++)
+        {
+            _putchar(a[row][col]);
+        }
+        _putchar('\n');
+    }
+}
